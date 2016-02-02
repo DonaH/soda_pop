426c9c8f20398220b0fb84ea1521aa30c0e6bb2a
dona <dona.hsieh@gmail.com>
2016-02-02 00:36:23 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+## This is a Macthing Soda Pop Game based on jquery.
+
+###Instructions:
+* Download all files.
+* Open index.html in a browser.
+* Click on the empty space to reveal the Soda pops.
+* Player has 60 seconds to match all soda pops.
+* 100 points for matching each pair.
+* Responsive design.
---
+++ script.js
@@ -0,0 +1,13 @@
+//var matchBottles = []
+
+$('.bottle').click(function(){
+  $(this).removeClass('secret')
+    console.log(this.id)
+})
+    // for (i = 0; i < 2; i += 1) {
+    //   $(this.id).append("matchBottles[i]")
+    // }
+
+// capture second click and check if first & second id
+// equal to each other
+// if no match, addClass 'secret'
