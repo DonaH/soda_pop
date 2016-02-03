135dc32caf05ab9279d9043ebe7c90636d190204
dona <dona.hsieh@gmail.com>
2016-02-02 19:25:30 -0800

    added scores, timer, & guess bottles

--- script.js
+++ script.js
@@ -1,13 +1,62 @@
-//var matchBottles = []
+var guesses = []
+var winCounter = 0
+var scoreKeeper = 0
+var timeKeeper = 60
 
+//game Initialize
+
+var timeStart = window.setInterval(decrementTimer, 1000)
+
+function decrementTimer() {
+  if (timeKeeper > 0 && timeKeeper < 61) {
+    timeKeeper -= 1
+    $('.timer').text(timeKeeper)
+    console.log(timeKeeper)
+  } else {
+      clearInterval(timeStart)
+    }
+}
+//$('#new_game').click(decrementTimer())
+//setInterval(decrementTimer, 1000)
+
+//start guessing the bottles
 $('.bottle').click(function(){
   $(this).removeClass('secret')
-    console.log(this.id)
-})
-    // for (i = 0; i < 2; i += 1) {
-    //   $(this.id).append("matchBottles[i]")
-    // }
 
-// capture second click and check if first & second id
-// equal to each other
-// if no match, addClass 'secret'
+    guesses.push(this)
+    console.log(guesses)
+    console.log($(guesses[0]).data('color'))
+    console.log($(guesses[1]).data('color'))
+
+    if (guesses.length === 2) {
+      if($(guesses[0]).data('color') === $(guesses[1]).data('color')){
+        console.log("they match")
+        $(guesses[0]).off('click')
+        $(guesses[1]).off('click')
+        guesses = []
+
+        //add score
+        scoreKeeper += 100
+        console.log(scoreKeeper)
+        $('.score').text(scoreKeeper)
+
+        //increment the winCounter by 1 until wholeboard is matched
+        winCounter += 1
+          if (winCounter === 8) {
+            alert('Congratulations!!! You win!')
+            winCounter === 0
+            clearInterval(timeStart)
+          }
+
+        } else {
+         console.log("they don't match")
+         $(guesses[0]).delay(300).queue(function(){
+           $(this).addClass('secret')
+         })
+         $(guesses[1]).delay(300).queue(function(){
+           $(this).addClass('secret')
+         })
+         guesses = []
+       }
+     }
+   })
