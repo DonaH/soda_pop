c904a31e9025a6811fa3235c3b362734cd2e9740
dona <dona.hsieh@gmail.com>
2016-02-04 07:08:41 -0800

    fixed minor bugs

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var guesses = []
 var winCounter = 0
 var scoreKeeper = 0
-var timeKeeper = 30
+var timeKeeper = 60
 var cleanBoard
 var timeStart
 var colorBottles = ['assets/soda_yellow.jpg', 'assets/soda_green.jpg', 'assets/soda_brown.jpg',
@@ -10,38 +10,16 @@ var colorBottles = ['assets/soda_yellow.jpg', 'assets/soda_green.jpg', 'assets/s
 'assets/soda_blue.jpg', 'assets/soda_orange.jpg', 'assets/soda_grey.jpg',
 'assets/soda_red.jpg', 'assets/soda_purple.jpg']
 
-// $('#new_game').on('click', game())
-
-// <div class="container">
-//   <div class="soda_board">
-//     <span><img class="bottle secret" data-color="soda_yellow" src="assets/soda_yellow.jpg" /></span>
 $('#new_game').on('click', function(){
-  shuffleBottles()
   setInterval('decrementTimer()', 1000)
+  shuffleBottles()
 })
 
-function decrementTimer() {
-  if (timeKeeper > 0 && timeKeeper < 31) {
-    timeKeeper -= 1
-    $('.timer').text(timeKeeper)
-    //console.log(timeKeeper)
-  } else {
-      if (winCounter === 8) {
-        reset()
-      }
-    }
-}
-//shuffle the bottles
-// var randomBottles = colorBottles[Math.floor(Math.random()*colorBottles.length)]
-
-//   <div class="soda_board">
-//     <span><img class="bottle secret" data-color="soda_yellow" src="assets/soda_yellow.jpg" /></span>
-
-var image_container = $('.soda_board img')
+var imageContainer = $('.soda_board img')
 function shuffleBottles() {
-  for (var i=0; i < image_container.length; i += 1){ // use a for loop to go through each img inside the soda_board
+  for (var i=0; i < imageContainer.length; i += 1){ // use a for loop to go through each img inside the soda_board
     var temp = Math.floor(Math.random()*colorBottles.length) // random index
-    $(image_container[i]).attr('src', colorBottles[temp]).attr('data-color', colorBottles[temp])
+    $(imageContainer[i]).attr('src', colorBottles[temp]).attr('data-color', colorBottles[temp])
     colorBottles.splice(temp,1)
   }
   colorBottles = ['assets/soda_yellow.jpg', 'assets/soda_green.jpg', 'assets/soda_brown.jpg',
@@ -51,16 +29,33 @@ function shuffleBottles() {
   'assets/soda_red.jpg', 'assets/soda_purple.jpg']
 }
 
+function decrementTimer() {
+  if (timeKeeper > 0 && timeKeeper < 61) {
+    timeKeeper -= 1
+    $('.timer').text(timeKeeper)
+    //console.log(timeKeeper)
+  } else {
+      if (winCounter == 8) {
+        reset()
+      }
+    }
+}
+
 $('.bottle').click(function(){
   $(this).removeClass('secret')
+
+  if(timeKeeper == 0){
+    alert("Time's up! Better luck next time.")
+    location.reload()
+  }
 
     guesses.push(this)
     console.log(guesses)
     console.log($(guesses[0]).data('color'))
     console.log($(guesses[1]).data('color'))
 
-    if (guesses.length === 2) {
-      if($(guesses[0]).data('color') === $(guesses[1]).data('color')){
+    if(guesses.length == 2) {
+      if(($(guesses[0]).data('color') == $(guesses[1]).data('color')) && ($('.timer')!= 0)){
         console.log("they match")
         $(guesses[0]).off('click')
         $(guesses[1]).off('click')
@@ -71,10 +66,8 @@ $('.bottle').click(function(){
         $('.score').text(scoreKeeper)
 
         winCounter += 1
-        if (winCounter === 8) {
+        if (winCounter == 8) {
           alert('Congratulations!!! You win!')
-          //reset()
-          timeKeeper = 0
           location.reload()
         }
       }
@@ -82,9 +75,12 @@ $('.bottle').click(function(){
         console.log("they don't match")
         $(guesses[0]).delay(200).queue(function(){
           $(this).addClass('secret')
+          console.log(this)
         })
         $(guesses[1]).delay(200).queue(function(){
           $(this).addClass('secret')
+          console.log(this)
+
         })
         guesses = []
     }
@@ -94,11 +90,10 @@ $('.bottle').click(function(){
 function reset() {
   guesses = []
   $('.score').text(0)
-  $('.timer').text(40)
+  $('.timer').text(60)
   scoreKeeper = 0
   clearInterval(timeStart)
-  timeKeeper = 40
+  timeKeeper = 60
   cleanBoard = ($('.bottle').addClass('secret'))
   winCounter = 0
-  //$('#new_game').on('click', game())
 }
